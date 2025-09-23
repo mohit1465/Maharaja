2938990d4693e1ebac06c2a063b6ae3d7a97faf8
Mohit1465 <Mohit8307521465@gmail.com>
2025-09-24 01:12:42 +0530

    done

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 // firebase-config.js
-const firebaseConfig = {
+const firebaseConfig = {  
     authDomain: "maharaja-fca8b.firebaseapp.com",
     projectId: "maharaja-fca8b",
     storageBucket: "maharaja-fca8b.firebasestorage.app",
     messagingSenderId: "193258828560",
     appId: "1:193258828560:web:2dc748cbe49df2468aab95",
     measurementId: "G-V92XEJ4XFR"
+    
   };
   
